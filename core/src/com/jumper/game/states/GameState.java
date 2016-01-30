164a9b3aca929b2ef757673e1bbc2afb156a5d27
Atanas Kostovski <atanasmk16@gmail.com>
2016-01-30 23:09:55 +0100

    Renames
    renaming it to jumper

---
+++ core/src/com/jumper/game/states/GameState.java
@@ -0,0 +1,14 @@
+package com.jumper.game.states;
+
+import java.util.Stack;
+
+/**
+ * Created by AtanasK on 30-Jan-16.
+ */
+public class GameState {
+    private Stack<GameState> states;
+
+    public GameState() {
+
+    }
+}
