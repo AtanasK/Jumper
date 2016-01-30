9b05a022081a86a57ed2ac9bf10a637cbc063ff3
Atanas Kostovski <atanasmk16@gmail.com>
2016-01-30 23:14:28 +0100

    Beginning of the states

--- core/src/com/jumper/game/states/GameState.java
+++ core/src/com/jumper/game/states/GameState.java
@@ -6,7 +6,7 @@ import java.util.Stack;
  * Created by AtanasK on 30-Jan-16.
  */
 public class GameState {
-    private Stack<GameState> states;
+    private Stack<State> states;
 
     public GameState() {
 
---
+++ core/src/com/jumper/game/states/State.java
@@ -0,0 +1,7 @@
+package com.jumper.game.states;
+
+/**
+ * Created by AtanasK on 30-Jan-16.
+ */
+public class State {
+}
